53012203bcf3d223d79edde8abbb36eaf1b0280a
cbust <acuario12543@gmail.com>
2026-04-29 13:00:14 -0600

    change today 3.1

--- src/main/java/com/charlycorporation/nutrition/security/JwtUtil.java
+++ src/main/java/com/charlycorporation/nutrition/security/JwtUtil.java
@@ -2,28 +2,33 @@ package com.charlycorporation.nutrition.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "charly-secret-key";
+    private final Key SECRET_KEY = Keys.hmacShaKeyFor(
+            "mi-super-secret-key-que-es-larga-y-segura-123456".getBytes()
+    );
 
     public String generateToken(String usuario){
 
         return Jwts.builder()
                 .setSubject(usuario)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2)) // 2 horas
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
     public String extractUsuario(String token){
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
