3442cb7995bb99e5f69e78ed33d324f51dabc713
cbust <acuario12543@gmail.com>
2026-05-01 11:04:04 -0600

    change friday

--- src/main/java/com/charlycorporation/nutrition/model/Usuario.java
+++ src/main/java/com/charlycorporation/nutrition/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.charlycorporation.nutrition.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class Usuario {
     @Column(unique = true)
     private String usuario;
 
+    @JsonIgnore
     private String password;
 
     private String rol; // ADMIN, COACH, etc
